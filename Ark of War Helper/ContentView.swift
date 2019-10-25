9e3849b622956c00d9b16f9184c490b4eb793f08
Brad Wang <bradwang60174@gmail.com>
2019-10-24 21:27:35 -0400

    Initial Commit

---
+++ Ark of War Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ark of War Helper
+//
+//  Created by Shuai Wang on 10/24/19.
+//  Copyright © 2019 Bright Ocean Technology Company. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ark of War HelperTests/Ark_of_War_HelperTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ark_of_War_HelperTests.swift
+//  Ark of War HelperTests
+//
+//  Created by Shuai Wang on 10/24/19.
+//  Copyright © 2019 Bright Ocean Technology Company. All rights reserved.
+//
+
+import XCTest
+@testable import Ark_of_War_Helper
+
+class Ark_of_War_HelperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
